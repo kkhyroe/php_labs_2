6b430bcd1dd05da841433f8cfb496699311aa10c
Roma <romachne@gmail.com>
2020-04-12 14:23:32 +0500

    lab2: test

--- lab2/index.php
+++ lab2/index.php
@@ -7,53 +7,6 @@
     </head>
 
     <body>
-        <?php
-            $x = -10;
-            $encounting = 10000;
-            $step = 2;
-            $type = 'A';
-
-            $min_value = 10;
-            $max_value = 20;    
-
-            if( $step == 'B' )
-                echo '<ul>';
-
-            for( $i=0; $i < $encounting; $i++, $x+=$step )
-            {   
-                if( $x == 0 )
-                    $f = 'error';
-
-                else
-                if( $x <= 10 )
-                    $f = 3/x + x/3 - 5;
-
-                else
-                if( $x <20 
-                    $f = (x-7) * (x/8);
-
-                else // иначе
-                if( x >= 22 )
-                    $f = 3*x + 2;
-
-                if( $step == 'A' )
-                {
-                    echo 'f('.$x.')='.$f;
-
-                    if( $i < $encounting-1 )
-                        echo '<br>';
-                }
-
-                else
-                if( $step == 'B' )
-                {
-                    echo '<li>f('. $x.')='.$f.'</li>';
-                }
-            }
-
-            if( $step == 'B' )
-                echo '</ul>';
-        ?>
 
         <header>
             <img src="img/logo_hw.png" alt="logo">
@@ -63,7 +16,53 @@
         </header>
 
         <main>
+            <?php
+                $x = -10;
+                $encounting = 10000;
+                $step = 2;
+                $type = 'A';
 
+                $min_value = 10;
+                $max_value = 20;    
+
+                if( $step == 'B' )
+                    echo '<ul>';
+
+                for( $i=0; $i < $encounting; $i++, $x+=$step )
+                {   
+                    if( $x == 0 )
+                        $f = 'error';
+
+                    else
+                    if( $x <= 10 )
+                        $f = 3/x + x/3 - 5;
+
+                    else
+                    if( $x <20 
+                        $f = (x-7) * (x/8);
+
+                    else // иначе
+                    if( x >= 22 )
+                        $f = 3*x + 2;
+
+                    if( $step == 'A' )
+                    {
+                        echo 'f('.$x.')='.$f;
+
+                        if( $i < $encounting-1 )
+                            echo '<br>';
+                    }
+
+                    else
+                    if( $step == 'B' )
+                    {
+                        echo '<li>f('. $x.')='.$f.'</li>';
+                    }
+                }
+
+                if( $step == 'B' )
+                    echo '</ul>';
+            ?>
         </main>
 
         <footer>
