42d685bf0f1bfcc4e41a347287dd97108398c09f
Roma <romachne@gmail.com>
2020-04-19 10:21:43 +0500

    lab4: test

--- lab4/index.php
+++ lab4/index.php
@@ -1,72 +1,72 @@
 <!DOCTYPE html>
 <html lang="ru">
     <head>
-        <title>Сычев Роман Александрович; 191-322; Лабораторная работа №А-</title>
+        <title>Сычев Роман Александрович; 191-322; Лабораторная работа №А-4</title>
         <link rel="stylesheet" href="style/style.css">
         <meta charset="UTF-8">
     </head>
 
     <body>
-        <header>
+        <header>    
             <img src="img/logo_hw.png" alt="logo">
             <p>Сычев Роман Александрович<br>
                 191-322<br>
-                Лабораторная работа №А-</p>
+                Лабораторная работа №А-4</p>
         </header>
 
         <main>
-            <div class="container">
-                <?php
-                $tdcount = 7;
+            <?php
+            $tdcount = 7;
         
-                $tables = array('С11*С12#С21', 'С11#С12#С13#С14#С15#С16#C17#C18', '', 'С11*С12*С13*С14*С15', '#', 'С11*С12*С13*С14*С15*С16*С17*С18', '*');
-        
-                if ($tdcount > 0) {
-                    for ($i = 0; $i < count($tables); $i++) {
-                        echo '<br><h2>Таблица №' . ($i + 1) . '</h2>';
-                        echo '<b>Структура таблицы:</b> <i>' . $tables[$i] . '</i><br><br>';
-        
-                        table($tables[$i]);
-                    }
-                } else echo 'Неправильное число колонок.';
-        
-                function table($construction) 
-                {
-                    $table = '';
-        
-                    $lines = explode('#', $construction);
-        
-                    for ($i = 0; $i < count($lines); $i++) $table .= getTR($lines[$i]);
-        
-                    if ($table != '') echo '<table border="1"cellpadding="10">' . $table . '</table>';
-                    else {
-                        if (strpbrk($construction, '#')) echo 'В таблице нет строк с ячейками <br>';
-                        else echo 'В таблице нет строк <br>';
-                    }
+            $tables = array('С11*С12#С21', 'С11#С12#С13#С14#С15#С16#C17#C18', '', 'С11*С12*С13*С14*С15', '#', 'С11*С12*С13*С14*С15*С16*С17*С18', '*');
+    
+            if ($tdcount > 0) {
+                for ($i = 0; $i < count($tables); $i++) {
+                    echo '<br><h2>Таблица №' . ($i + 1) . '</h2>';
+                    echo '<b>Структура таблицы:</b> <i>' . $tables[$i] . '</i><br><br>';
+    
+                    table($tables[$i]);
                 }
-        
-                function getTR($line)
-                {
-                    global $tdcount;
-        
-                    if ($line != '') {
-                        $td = explode('*', $line);
-                        $tds = '';
-        
-                        for ($j = 0; $j < $tdcount; $j++) {
-                            $tds .= '<td>' . $td[$j] . '</td>';
-                        }
-        
-                        if ($tds != '') return '<tr>' . $tds . '</tr>';
-                    }
+            } else echo 'Неправильное число колонок.';
+    
+            function table($construction) 
+            {
+                $table = '';
+    
+                $lines = explode('#', $construction);
+    
+                for ($i = 0; $i < count($lines); $i++) $table .= getTR($lines[$i]);
+    
+                if ($table != '') echo '<table border="1"cellpadding="10">' . $table . '</table>';
+                else {
+                    if (strpbrk($construction, '#')) echo 'В таблице нет строк с ячейками <br>';
+                    else echo 'В таблице нет строк <br>';
                 }
-        
-                ?>
-            </div>
+            }
+    
+            function getTR($line)
+            {
+                global $tdcount;
+    
+                if ($line != '') {
+                    $td = explode('*', $line);
+                    $tds = '';
+    
+                    for ($j = 0; $j < $tdcount; $j++) {
+                        $tds .= '<td>' . $td[$j] . '</td>';
+                    }
+    
+                    if ($tds != '') return '<tr>' . $tds . '</tr>';
+                }
+            }
+    
+            ?>
         </main>
 
         <footer>
-            
+            <?php
+                echo 'Установленное количество колонок: ' . $tdcount . '<br>';
+            ?>
         </footer>
     </body>
 </html>
